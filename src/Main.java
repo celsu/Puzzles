22a8eeafd92faa5292ef7d507e78bb79f30a980a
Celso Machado <celsobarrosmachado@gmail.com>
2023-08-03 11:54:50 +0100

    Update

--- src/Main.java
+++ src/Main.java
@@ -3,33 +3,40 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
+        //Scanner in = new Scanner(System.in);
 
         ArrayList<Integer> li = new ArrayList<Integer>();
-
+        li.add(2);
+        li.add(3);
+        li.add(7);
+        li.add(5);
+        li.add(1);
+        li.add(0);
+        li.add(8);
+        li.add(4);
 
         int maxAt = 0;
 
         // game loop
-        while (true) {
 
             for (int i = 0; i < 8; i++) {
-                int mountainH = in.nextInt(); // represents the height of one mountain.
-                li.add(mountainH);
-                System.err.println(li);
+                //int mountainH = in.nextInt(); // represents the height of one mountain.
+                //li.add(mountainH);
+                //System.err.println(li);
             }
-            //List<Integer> sortedList = li.stream().sorted().collect(Collectors.toList());
-            //System.err.println(sortedList);
-
-            //sortedList = li.stream().sorted().collect(Collectors.toList());
-            //System.out.println(5);
-
+            //PEGA O INDEX DO MAIOR VALOR *PEGA O INDEX, NAO O VALOR*
             for (int i = 0; i < li.size(); i++) {
+                //VERIFICA
                 maxAt = li.get(i) > li.get(maxAt) ? i : maxAt;
+
+                //System.out.println("maxt: "+maxAt);
+                //System.out.println("i: "+i);
             }
             System.out.println(maxAt);
             li.clear();
 
-        }
+            //HELP STRREAM
+            //List<Integer> sortedList = li.stream().sorted().collect(Collectors.toList());
+
     }
 }
