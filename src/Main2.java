cc7518000322b32b697198bac3fe95b53e7e2ab2
Celso Machado <celsobarrosmachado@gmail.com>
2023-08-05 23:52:56 +0100

    Update

--- src/Main2.java
+++ src/Main2.java
@@ -3,12 +3,30 @@ import java.util.ArrayList;
 public class Main2 {
     public static void main(String args[]) {
 
-        String s = "Batteryi";
+        //https://docs.oracle.com/javase/8/docs/api/?java/lang/StringBuilder.html
 
-        StringBuilder type_hold = new StringBuilder(s);
-        long vowel_num = type_hold.toString().toLowerCase().chars()
-                .filter(it -> "aeiou".indexOf(it) != -1).count();
+        String s = "Battery";
 
-        System.out.println(vowel_num);
+        StringBuilder sb = new StringBuilder(s);
+        int soma = 0;
+
+
+        for (int i = 0; i < sb.length(); i++) {
+            //System.out.println(sb.charAt(i));
+            if(sb.charAt(i) == 'a') soma = soma + 1;
+            if(sb.charAt(i) == 'e') soma = soma + 2;
+            if(sb.charAt(i) == 'i') soma = soma + 3;
+            if(sb.charAt(i) == 'o') soma = soma + 4;
+            if(sb.charAt(i) == 'u') soma = soma + 5;
+        }
+
+        System.out.println("A soma e: "+soma);
+
+
+
+
+
+        //Metodo Chars retorna Int
+        //long vowel_num = type_hold.toString().toLowerCase().chars().filter(it -> "aeiou".indexOf(it) != -1).count();
     }
 }
