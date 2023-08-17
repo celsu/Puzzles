f29529c091e8dff4e87584d7003057a8422f8330
Celso Machado <celsobarrosmachado@gmail.com>
2023-08-17 12:32:37 +0100

    Update

--- src/Main2.java
+++ src/Main2.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main2 {
     public static void main(String args[]) {
@@ -77,6 +78,20 @@ public class Main2 {
     //VERIFICAR NUMERO IMPAR
         ArrayList <Integer> l1 = new ArrayList<Integer>();
         List<Integer> l2 = new ArrayList<Integer>();
+
+        l1.add(1);
+        l1.add(2);
+        l1.add(3);
+        l1.add(4);
+        l1.add(5);
+
+        for(int i=0; i<l1.size(); i++){
+            if(l1.get(i)%2==0){
+                l1.remove(l1.get(i));
+            }
+        }
+
+        l1.stream().filter(h -> h%2!=0).forEach(System.out::println);
 
         l1.add(1);
         l1.add(2);
